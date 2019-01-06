2191f274be30dec8ff78610ac63dd557fad46640
jc5311 <jc.psp5@gmail.com>
2019-01-06 14:05:13 -0500

    bug fixed! Important boolean was made global and serialRcv loop would be skipped entirely. So additional updates were never made, hence the led getting stuck.

--- slave/slave.ino
+++ slave/slave.ino
@@ -8,8 +8,6 @@
   char id = '3';
   int state = 0;
   uint8_t color_array[3] = {0, 0, 0}; //initialize to white
-  boolean data_available = false;
-  uint8_t return_buffer[DATA_LENGTH] = {255, 255, 255, 255};
 
 
 
@@ -26,51 +24,57 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {  
+void loop() {
   //if serial device is available, read in the data
   if (Serial.available() > 0){
+    Serial.println("Calling serialRcv()");
     serialRcv();
     Serial.println("---Color array values as seen by loop()---");
     Serial.println(color_array[0], DEC);
     Serial.println(color_array[1], DEC);
     Serial.println(color_array[2], DEC);
+    Serial.println("---Writing to leds---");
     analogWrite(9, color_array[0]);
     analogWrite(10, color_array[1]);
     analogWrite(11, color_array[2]);
+    Serial.println("\n\n\n\n\n");
   }//end of serial available check
 
   
-} //end of loopPACKET_LENGTH
+} //end of loop
 
 //receive data from master and update color array
 void serialRcv(void){
+  Serial.println("Entered serialRcv()");
   //initialize neccesary variables
-  boolean rcv_in_progress = false;
+  uint8_t rcv_in_progress = FALSE;
+  uint8_t data_available = FALSE;
   uint8_t loop_index = 0;
   uint8_t data_index = 0;
   uint8_t rb;
+  uint8_t return_buffer[DATA_LENGTH] = {255, 255, 255, 255};
 
   //loop so long as serial data is still available
   //and all of it hasn't been collected yet
-  while ((Serial.available() > 0) || (data_available == FALSE)){
+  while ((Serial.available() > 0) && (data_available == FALSE)){
     //capture data right away
     rb = Serial.read();
 
     //are we actively reading data? if so, continue processing it
-    if (rcv_in_progress){
-      Serial.println("---------");
-      Serial.println("Entered rcv_in_progress block");
+    if (rcv_in_progress == TRUE){
       //check if the datum is the end byte and close up shop if so
       if (rb == 0xBB){
+        Serial.println("End byte found, setting data_available to TRUE");
         rcv_in_progress = FALSE;
         data_available = TRUE;
       }
 
       //otherwise save the data and continue
       else{
-        Serial.println("---------");
-        Serial.println("rb as seen by SerialRcv assignment block");
+        Serial.println("rb= ");
         Serial.println(rb, HEX);
+        Serial.println("data_index= ");
+        Serial.println(data_index, DEC);
         return_buffer[data_index] = rb;
         data_index++;
       }
@@ -78,6 +82,7 @@ void serialRcv(void){
 
     //if not then is the byte we just read the start byte?
     else if (rb == 0xAA){
+      Serial.println("Start byte found, setting rcv_in_progress to TRUE");
       //acknowledge the start bit and start looking for incoming data
       rcv_in_progress = TRUE;
     }
@@ -87,11 +92,11 @@ void serialRcv(void){
     //we receive serial data and the buffer is full, the arduino will sift
     //through the data until a start bit is found in case a packet was properly
     //sent. Otherwise the function should throw an exception.
+  
+  } //end of while loop
 
-  }
-
-  /*if (data_available == true){*/
-    Serial.println("---------");
+  if (data_available == TRUE){
+    Serial.print("loop ended and data_available is TRUE, filling color_array");
     Serial.println("return_buffer[1]: ");
     Serial.println(return_buffer[1], HEX);
     Serial.println("return_buffer[2]: ");
@@ -101,9 +106,6 @@ void serialRcv(void){
     color_array[0] = return_buffer[1];
     color_array[1] = return_buffer[2];
     color_array[2] = return_buffer[3];
-    /*return;
   }
-  else{
     return;
-  }*/
 }
