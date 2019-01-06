0c8e05f80a114445a847c8e67a089a55cd44ebd3
jc5311 <jc.psp5@gmail.com>
2019-01-06 15:35:23 -0500

    Removed debug comments and print statements

--- slave/slave.ino
+++ slave/slave.ino
@@ -27,17 +27,10 @@ void setup() {
 void loop() {
   //if serial device is available, read in the data
   if (Serial.available() > 0){
-    Serial.println("Calling serialRcv()");
     serialRcv();
-    Serial.println("---Color array values as seen by loop()---");
-    Serial.println(color_array[0], DEC);
-    Serial.println(color_array[1], DEC);
-    Serial.println(color_array[2], DEC);
-    Serial.println("---Writing to leds---");
     analogWrite(9, color_array[0]);
     analogWrite(10, color_array[1]);
     analogWrite(11, color_array[2]);
-    Serial.println("\n\n\n\n\n");
   }//end of serial available check
 
   
@@ -45,7 +38,6 @@ void loop() {
 
 //receive data from master and update color array
 void serialRcv(void){
-  Serial.println("Entered serialRcv()");
   //initialize neccesary variables
   uint8_t rcv_in_progress = FALSE;
   uint8_t data_available = FALSE;
@@ -64,17 +56,13 @@ void serialRcv(void){
     if (rcv_in_progress == TRUE){
       //check if the datum is the end byte and close up shop if so
       if (rb == 0xBB){
-        Serial.println("End byte found, setting data_available to TRUE");
+        //End byte found, setting data_available to TRUE
         rcv_in_progress = FALSE;
         data_available = TRUE;
       }
 
       //otherwise save the data and continue
       else{
-        Serial.println("rb= ");
-        Serial.println(rb, HEX);
-        Serial.println("data_index= ");
-        Serial.println(data_index, DEC);
         return_buffer[data_index] = rb;
         data_index++;
       }
@@ -82,16 +70,10 @@ void serialRcv(void){
 
     //if not then is the byte we just read the start byte?
     else if (rb == 0xAA){
-      Serial.println("Start byte found, setting rcv_in_progress to TRUE");
+      //Start byte found, setting rcv_in_progress to TRUE
       //acknowledge the start bit and start looking for incoming data
       rcv_in_progress = TRUE;
     }
-
-    //do nothing if we receive data thats not the start bit and if we are not
-    //actively seeking additional data. What this will cause is that anytime
-    //we receive serial data and the buffer is full, the arduino will sift
-    //through the data until a start bit is found in case a packet was properly
-    //sent. Otherwise the function should throw an exception.
   
   } //end of while loop
 
