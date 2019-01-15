afc13e02010c8291bfd305dcc70d35e40c8cebdc
jc5311 <jc.psp5@gmail.com>
2019-01-15 08:25:08 -0500

    * Added globals to handle when/when not to animate * Added configuration for interrupt pin * Added prototype for interrupt ISR * Added prototype for end of timer routine associated with rcvrISR

--- master/master.ino
+++ master/master.ino
@@ -1,10 +1,18 @@
+//defines
 #define PACKET_LENGTH 6
 #define NORMAL_RANGE 1
 #define FLIPPED_RANGE 0
 #define EIGHTBITMAX 255
 
+//globals
+uint8_t dont_animate = 0;
+uint8_t interrupt_pin = 2;
+
 void setup() {
-  // put your setup code here, to run once:
+  //initialize interrupt pin and configure pullup resistor
+  pinMode(interrupt_pin, INPUT_PULLUP);
+  //attach interrupt to the interrupt pin and configure trigger on falling edge
+  attachInterrupt(digitalPinToInterrupt(interrupt_pin), rcvrISR, FALLING);
 
   //setup serial connection
   Serial.begin(115200);
@@ -12,21 +20,26 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
- //loop and send a message to everyone
- setBarColor(0xAB, 232, 12, 122, NORMAL_RANGE);
- delay(250);
- setBarColor(0xBC, 0, 255, 0, NORMAL_RANGE);
- delay(250);
- setBarColor(0xCD, 0, 0, 255, NORMAL_RANGE);
- delay(250);
- setBarColor(0xAB, 0, 0, 0, NORMAL_RANGE);
- delay(250);
- setBarColor(0xBC, 0, 0, 0, NORMAL_RANGE);
- delay(250);
- setBarColor(0xCD, 0, 0, 0, NORMAL_RANGE);
- delay(250);
+  
+  if (dont_animate){
+    //Do nothing. After 5 second timer end dont_animate should reset to 0 and
+    //animations will restart
+  }
+  else{
+    //loop and send animation messages to everyone
+    setBarColor(0xAB, 232, 12, 122, NORMAL_RANGE);
+    delay(250);
+    setBarColor(0xBC, 0, 255, 0, NORMAL_RANGE);
+    delay(250);
+    setBarColor(0xCD, 0, 0, 255, NORMAL_RANGE);
+    delay(250);
+    setBarColor(0xAB, 0, 0, 0, NORMAL_RANGE);
+    delay(250);
+    setBarColor(0xBC, 0, 0, 0, NORMAL_RANGE);
+    delay(250);
+    setBarColor(0xCD, 0, 0, 0, NORMAL_RANGE);
+    delay(250);
+  }
 }
 
 //wrapper function to send color message
@@ -51,4 +64,20 @@ void setBarColor(uint8_t bar_addr, uint8_t red, uint8_t green, uint8_t blue, uin
 
   //Write buffer to serial
   Serial.write(buffer, sizeof(buffer));
+}
+
+//function to trigger animation disabling and 5 second timeout
+//on receiver interrupt
+void rcvrISR(void){
+  //function call to disable animations
+
+  //set loop condition to not perform animations
+
+  //begin or reset 5 second timer
+}
+
+//function to restart animations after timer end
+void timerRoutine(void){
+  //restart animations
+  dont_animate = 0;
 }
