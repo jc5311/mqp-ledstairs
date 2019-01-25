d35953974b5f40f9d83cd9abb77a5cea156a9f74
juan <jc.psp5@gmail.com>
2019-01-25 04:24:39 -0500

    Added ADC code to animation loop for the sake of demoing it working.

--- master/master.ino
+++ master/master.ino
@@ -35,12 +35,14 @@
 //globals
 uint8_t debug_led = 13;
 uint8_t debug_toggle = 0;
-uint8_t interrupt_pin = 2; //only p2 and p3 can be used for interrupt on nano
+uint8_t rcvr_interrupt_pin = 2; //only p2 and p3 can be used for interrupt on nano
+uint8_t adc_interrupt_pin = 3; //only p2 and p3 can be used for interrupt on nano
 uint8_t led_bar[LED_BAR_COUNT]; //array to hold led bar addresses
 uint8_t timer_done = 0;
 uint8_t timer_counter = 0;
 uint8_t timeout_counter = 0;
 uint8_t toggler = 0;
+float light_dimness = 1;
 
 //prototypes
 void setBarColor(uint8_t bar_addr, uint8_t red, 
@@ -49,11 +51,13 @@ void disableLedBars(uint8_t led_bar_count, uint8_t led_bar[]);
 void rcvrISR(void);
 void cooldownTimer(void);
 //rtos tasks
-void TaskAnimate(void *pvParameters);
-void TaskAnimationDisable(void *pvParameters);
+void TaskAnimate(void* pvParameters);
+void TaskAnimationDisable(void* pvParameters);
+void TaskReadAdcBrightness(void* pvParameters);
 
 //rtos semaphores
 SemaphoreHandle_t xLedDisableSemaphore;
+SemaphoreHandle_t xAdcUpdateSemaphore;
 
 void setup() {
   //debug led setup
@@ -61,9 +65,12 @@ void setup() {
   digitalWrite(debug_led, LOW);
 
   //initialize interrupt pin and configure pullup resistor
-  pinMode(interrupt_pin, INPUT_PULLUP);
+  pinMode(rcvr_interrupt_pin, INPUT_PULLUP);
   //attach interrupt to the interrupt pin and configure trigger on falling edge
-  attachInterrupt(digitalPinToInterrupt(interrupt_pin), rcvrISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(rcvr_interrupt_pin), rcvrISR, FALLING);
+  //do the same for the adc for demoing
+  pinMode(adc_interrupt_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(adc_interrupt_pin), adcISR, FALLING);
 
   //setup serial connection
   Serial.begin(BAUD_RATE);
@@ -74,9 +81,12 @@ void setup() {
   led_bar[1] = 0xBC;
   led_bar[2] = 0xCD;
 
-  //configure semaphore
+  //configure semaphores
   if (xLedDisableSemaphore == NULL){ //confirm semaphore wasn't already made
     xLedDisableSemaphore = xSemaphoreCreateBinary();
+  }
+  if (xAdcUpdateSemaphore == NULL){ //confirm semaphore wasn't already made
+    xAdcUpdateSemaphore = xSemaphoreCreateBinary();
   }
 
   //configure RTOS tasks
@@ -95,6 +105,15 @@ void setup() {
     128, //stack size
     NULL,
     2, //priority
+    NULL
+  );
+
+  xTaskCreate(
+    TaskReadAdcBrightness,
+    (const portCHAR *)"Read ambient brightness",
+    128, //stack size
+    NULL,
+    3, //priority
     NULL
   );
 }
@@ -117,9 +136,9 @@ void setBarColor(uint8_t bar_addr, uint8_t red, uint8_t green, uint8_t blue, uin
   //if leds are common cathode pass color values as-is
   if (range_type == NORMAL_RANGE){
     buffer[1] = bar_addr;
-    buffer[2] = red;
-    buffer[3]= green;
-    buffer[4] = blue;
+    buffer[2] = red * light_dimness;
+    buffer[3]= green * light_dimness;
+    buffer[4] = blue * light_dimness;
   }
   //if leds are common anode reverse the color values
   else if (range_type == FLIPPED_RANGE){
@@ -149,6 +168,11 @@ void rcvrISR(void){
   xSemaphoreGiveFromISR(xLedDisableSemaphore, NULL);
 } //end of rcvrISR()
 
+void adcISR(void){
+  //post adc_update semaphore
+  xSemaphoreGiveFromISR(xAdcUpdateSemaphore, NULL);
+}
+
 //interrupt service routine for timer 2
 ISR (TIMER2_OVF_vect){
   timer_counter++;
@@ -175,6 +199,32 @@ void TaskAnimate(void *pvParameters){
   (void) pvParameters;
 
   while(1){
+    //record ambient_brightness
+  uint16_t reading;
+  reading = analogRead(A0);
+  //reading = 200;
+
+  //calculate the dimming scale
+  if (reading < 205){ 
+    //100% brightness
+    light_dimness = 1.0;
+  }
+  else if ((reading > 205) && (reading <= 410)){
+    //80% brightness
+    light_dimness = 0.8;
+  }
+  else if ((reading > 410) && (reading <= 615)){
+    //60% brightness
+    light_dimness = 0.5;
+  }
+  else if ((reading > 615) && (reading <= 820)){
+    //40% brightness
+    light_dimness = 0.4;
+  }
+  else{
+    //20% brightness
+    light_dimness = 0.2;
+  }
     //loop and send animation messages to everyone
     setBarColor(led_bar[0], 232, 12, 122, NORMAL_RANGE);
     delay(250);
@@ -216,6 +266,7 @@ void cooldownTimer(void){
   //loop until timer complete
   digitalWrite(debug_led, HIGH);
   while (!timer_done);
+  digitalWrite(debug_led, HIGH);
   timer_done = 0;
   return;
 }
@@ -225,28 +276,36 @@ void cooldownTimer(void){
  * Perform an analogRead() and return a scale by which
  * LED brightness must be reduced.
  */ 
-uint16_t readAmbientBrightness(void){
+void TaskReadAdcBrightness(void* pvParameters){
+  (void) pvParameters;
+
+  //pend AdcUpdateSemaphore
+  xSemaphoreTake(xAdcUpdateSemaphore, portMAX_DELAY);
+
   //record ambient_brightness
-  uint8_t dimness = 0;
-  uint16_t reading = analogRead(A0);
+  uint16_t reading;
+  //reading = analogRead(A0)
+  reading = 620;
 
   //calculate the dimming scale
   if (reading < 205){ 
     //100% brightness
-    dimness = 1;
+    light_dimness = 1.0;
   }
   else if ((reading > 205) && (reading <= 410)){
     //80% brightness
+    light_dimness = 0.8;
   }
   else if ((reading > 410) && (reading <= 615)){
     //60% brightness
+    light_dimness = 0.5;
   }
   else if ((reading > 615) && (reading <= 820)){
     //40% brightness
+    light_dimness = 0.4;
   }
   else{
     //20% brightness
+    light_dimness = 0.2;
   }
-
-
 }
