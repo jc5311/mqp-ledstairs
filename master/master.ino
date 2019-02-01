3a09c51aa5d26df266db30e4756bee7e7f2ca91c
JC5311 <jc.psp5@gmail.com>
2019-02-01 02:24:54 -0500

    Big changes. Added system low_power mode that plays animation for two minutes and disables them for 8 when no receiver interrupts have occured for 30 minutes.

--- master/master.ino
+++ master/master.ino
@@ -32,6 +32,8 @@
 #define EIGHTBITMAX 255
 #define BAUD_RATE 115200 //master and slave mcu bauds MUST match
 #define COOLDOWN_PERIOD 5 //timer cooldown period (in seconds)
+#define TRUE 1
+#define FALSE 0
 
 //globals
 uint8_t debug_led = 13;
@@ -39,13 +41,18 @@ uint8_t debug_toggle = 0;
 uint8_t rcvr_interrupt_pin = 2; //only p2 and p3 can be used for interrupt on nano
 uint8_t adc_interrupt_pin = 3; //only p2 and p3 can be used for interrupt on nano
 uint8_t led_bar[LED_BAR_COUNT]; //array to hold led bar addresses
-//variables used in interrupts must ALWAYS be declared as volatile
-volatile uint8_t timer_done = 0;
-volatile uint8_t timer_counter = 0;
-volatile uint8_t timeout_counter = 0;
 uint8_t toggler = 0;
 float light_dimness = 1;
-volatile uint8_t timer_already_running = 0;
+//variables used in interrupts must ALWAYS be declared as volatile
+volatile uint8_t cooldown_done = 0;
+volatile uint8_t timer_counter = 0;
+volatile uint8_t cooldown_counter = 0;
+volatile uint16_t time_since_last_rcvr_int = 0;
+volatile uint16_t lowpower_counter = 0;
+volatile uint8_t timer_already_running = FALSE;
+volatile uint8_t animation_disable = FALSE;
+volatile uint8_t lowpower_sleep = FALSE; //use for actual sleeping during low power mode
+volatile uint8_t lowpower_mode_active = FALSE;
 
 //prototypes
 void setBarColor(uint8_t bar_addr, uint8_t red, 
@@ -61,6 +68,9 @@ void TaskReadAdcBrightness(void* pvParameters);
 //rtos semaphores
 SemaphoreHandle_t xLedDisableSemaphore;
 SemaphoreHandle_t xAdcUpdateSemaphore;
+//rtos task handles
+TaskHandle_t xAnimateHandle;
+
 
 void setup()
 {
@@ -68,7 +78,7 @@ void setup()
   pinMode(debug_led, OUTPUT);
   digitalWrite(debug_led, LOW);
 
-  //initialize interrupt pin and configure pullup resistor
+  /********** Initialize and configure interrupts **********/
   pinMode(rcvr_interrupt_pin, INPUT_PULLUP);
   //attach interrupt to the interrupt pin and configure trigger on falling edge
   attachInterrupt(digitalPinToInterrupt(rcvr_interrupt_pin), rcvrISR, FALLING);
@@ -76,16 +86,16 @@ void setup()
   pinMode(adc_interrupt_pin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(adc_interrupt_pin), adcISR, FALLING);
 
-  //setup serial connection
+  /********** Configure and start serial port **********/
   Serial.begin(BAUD_RATE);
   while(!Serial); //wait for serial port to connect
 
-  //fill led bar array with led bar addresses
+  /********** Initialize led bar addresses **********/
   led_bar[0] = 0xAB;
   led_bar[1] = 0xBC;
   led_bar[2] = 0xCD;
 
-  //configure semaphores
+  /********** Configure Semaphores **********/
   if (xLedDisableSemaphore == NULL)
   { 
     //confirm semaphore wasn't already made
@@ -97,14 +107,21 @@ void setup()
     xAdcUpdateSemaphore = xSemaphoreCreateBinary();
   }
 
-  //configure RTOS tasks
+  /********** Configure and start system timer **********/
+  TCCR2B = 0 << CS20; //timer clock => disable for configuration
+  TIFR2 = 1 << TOV2; //clear the overflow flag
+  TIMSK2 = 1 << TOIE2; //enable timer interrupts
+  TCNT2 = 0x00; //clear timer0 counter
+  TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20); //start timer with 1024 prescaler
+
+  /********** Configure RTOS Tasks **********/
   xTaskCreate(
     TaskAnimate,
     (const portCHAR *)"Toggle Animations",
     128, //stack size
     NULL,
     1, //priority
-    NULL
+    &xAnimateHandle
   );
 
   xTaskCreate(
@@ -131,7 +148,7 @@ void loop()
   //All work is done in RTOS tasks. No code should be placed here!
 }
 
-/***** System Control Functions *****/
+/********** System Control Functions **********/
 
 void setBarColor(uint8_t bar_addr, uint8_t red, uint8_t green, uint8_t blue, uint8_t range_type){
   //pend semaphore
@@ -176,12 +193,17 @@ void disableLedBars(uint8_t led_bar_count, uint8_t led_bar[])
 void rcvrISR(void){
   if (!timer_already_running)
   {
+    //absolutely disable lowpower_mode
+    lowpower_mode_active = FALSE;
+    lowpower_counter = 0;
+    time_since_last_rcvr_int = 0;
     //post led_disable_semaphore
     xSemaphoreGiveFromISR(xLedDisableSemaphore, NULL);
   }
   else //if timer is currently active reset its counter
   {
-    timeout_counter = 0;
+    time_since_last_rcvr_int = 0;
+    cooldown_counter = 0;
     timer_counter = 0;
   }
   
@@ -197,24 +219,62 @@ ISR (TIMER2_OVF_vect)
 {
   timer_counter++;
 
-  if (timer_counter == 30){
-    timeout_counter++;
+  //if a second has passed
+  if (timer_counter == 30)
+  {
+    //reset the one second signal
     timer_counter = 0;
-  }
 
-  if (timeout_counter == COOLDOWN_PERIOD){
-    digitalWrite(debug_led, LOW);
-    TCCR2B = (0 << CS22) | (0 << CS21) | (0 << CS20); //disable timer
-    TCNT2 = 0x00; //clear counter
+    //incremement and check counter for time since last receiver interrupt
+    time_since_last_rcvr_int++;
+    if (time_since_last_rcvr_int == 1800) //if 30m have passed
+    {
+      lowpower_mode_active = TRUE;
+      lowpower_counter = 0;
+      lowpower_sleep = FALSE;
+    }
+    
+    if (lowpower_mode_active == TRUE)
+    {
+      if (lowpower_sleep)
+      {
+        if (lowpower_counter == 480) //if 8 minutes have passed
+        {
+          lowpower_counter = 0;
+          lowpower_sleep = FALSE; //disable animation waiting
+          vTaskResume(xAnimateHandle); //resume animation task
+        }
+      }
+      else //if we aren't waiting to resume animation
+      {
+        if (lowpower_counter == 120) //if two minutes of animation has passed
+        {
+          lowpower_counter = 0;
+          lowpower_sleep = TRUE;
+          vTaskSuspend(xAnimateHandle); //suspend animation task
+        }
+      }
+      lowpower_counter++;
+    }
+
+    //increment and check count for time since last adc read
+
+    //when disable animation task is active
+    if (animation_disable == TRUE)
+    {
+      //do all animation disable (trip sensor) stuff here
+      cooldown_counter++;
+      if (cooldown_counter == COOLDOWN_PERIOD){
+        cooldown_counter = 0;
+        cooldown_done = 1; //signal that COOLDOWN_PERIOD seconds have passed
+      }
+    }
+
+    //this might go here, this is for resetting the one second signal
     timer_counter = 0;
-    timeout_counter = 0;
-    timer_done = 1; //signal that COOLDOWN_PERIOD seconds have passed
-    //Serial.println(2);
   }
 
 }
-
-/***** RTOS Tasks *****/
 
 //task to toggle through and execute animations
 void TaskAnimate(void *pvParameters __attribute__((unused)) ){
@@ -251,26 +311,22 @@ void TaskAnimationDisable(void *pvParameters __attribute__((unused)) )
       //signal that timeout is currently running, this fixes bug where
       //2 successive interrupts cause a 10 second cooldown instead of 5 after
       //the last
-      timer_already_running = 1;
+      timer_already_running = TRUE;
       
       //loop through led bars and send message to disable animation
       disableLedBars(LED_BAR_COUNT, led_bar);
       
-      //delay COOLDOWN_PERIOD seconds
-      TCCR2B = 0 << CS20; //timer clock => disable for configuration
-      TIFR2 = 1 << TOV2; //clear the overflow flag
-      TIMSK2 = 1 << TOIE2; //enable timer interrupts
-      TCNT2 = 0x00; //clear timer0 counter
-      TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20); //start timer with 1024 prescaler
+      //set signal for timer to acknowledge animation disable
+      animation_disable = TRUE;
 
       //loop until timer complete
-      digitalWrite(debug_led, HIGH);
-      vTaskSuspendAll();
-      while (timer_done != 1);
-      xTaskResumeAll();
-      timer_done = 0;
+      vTaskSuspend(xAnimateHandle); //suspend animation task
+      while (cooldown_done != TRUE);
+      vTaskResume(xAnimateHandle); //resume animation task
+      cooldown_done = FALSE; //reset the cooldown_done signal
     }
-    timer_already_running = 0;
+    timer_already_running = FALSE;
+    animation_disable = FALSE;
   }
 }
 
